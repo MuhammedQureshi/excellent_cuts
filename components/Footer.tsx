bd4647a7f190d21229277cd960149e6a4bd2797e
Muhammed Qureshi <muhammed1qureshi@gmail.com>
2025-10-28 17:38:22 +0000

    upd prices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export function Footer() {
               </span>
             </div>
             <p className="text-white/70">
-              Where tradition meets style. Your neighborhood barber shop since 1995.
+              Where tradition meets style. Your neighborhood barber shop since 2003.
             </p>
           </div>
 
